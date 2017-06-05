a835844733f6b82168f067171fdc5929f0ad5eb3
yahan <yahan.k@gmail.com>
2017-06-05 21:17:38 +0300

    first commit

---
+++ ProgDVB/remote.lua
@@ -0,0 +1,147 @@
+﻿local win = libs.win;
+local kb  = libs.keyboard;
+local on  = 0;
+
+actions.switch = function()
+  win.switchtowait("ProgDvbNet.exe", 2000);
+end
+
+actions.power = function ()
+  if (on==0) then
+    os.start("C:\\Program Files\\ProgDVB\\ProgDvbNet.exe");
+    on = 1;
+  else
+    actions.switch();
+    kb.stroke("X");
+    on = 0;
+  end;
+end
+
+actions.vmute = function ()
+  actions.switch();
+  kb.stroke("M");
+end
+
+actions.vup = function ()
+  kb.stroke("volumeup");
+end
+
+actions.vdown = function ()
+  kb.stroke("volumedown");
+end
+
+actions.pch = function ()
+  actions.switch();
+  kb.stroke("up");
+end
+
+actions.nch = function ()
+  actions.switch();
+  kb.stroke("down");
+end
+
+actions.left = function ()
+  actions.switch();
+  kb.stroke("left");
+end
+
+actions.right = function ()
+  actions.switch();
+  kb.stroke("right");
+end
+
+actions.ok = function ()
+  actions.switch();
+  kb.stroke("enter");
+end
+
+actions.exit = function ()
+  actions.switch();
+  kb.stroke("esc");
+end
+
+actions.menu = function ()
+  actions.switch();
+  kb.stroke("O");
+end
+
+actions.fullscreen = function ()
+  actions.switch();
+  kb.stroke("F");
+end
+
+actions.info = function ()
+  actions.switch();
+  kb.stroke("I");
+end
+
+actions.epg = function ()
+  actions.switch();
+  kb.stroke("E");
+end
+
+actions.zoom = function ()
+  actions.switch();
+  kb.stroke("Z");
+end
+
+actions.pgup = function ()
+  actions.switch();
+  kb.stroke("pgup");
+end
+
+actions.pgdn = function ()
+  actions.switch();
+  kb.stroke("pgdown");
+end
+
+actions.ratio = function ()
+  actions.switch();
+  kb.stroke("V");
+end
+
+actions.lbox = function ()
+  actions.switch();
+  kb.stroke("B");
+end
+
+actions.pip = function ()
+  actions.switch();
+  kb.stroke("P");
+end
+
+actions.nopip = function ()
+  actions.switch();
+  kb.stroke("ctrl", "P");
+end
+
+actions.stop = function ()
+  actions.switch();
+  kb.stroke("ctrl", "alt", "S");
+end
+
+actions.play = function ()
+  actions.switch();
+  kb.stroke("ctrl", "alt", "P");
+end
+
+actions.pause = function ()
+  actions.switch();
+  kb.stroke("ctrl", "alt", "B");
+end
+
+actions.record = function ()
+  actions.switch();
+  kb.stroke("R");
+end
+
+actions.rwd = function ()
+  actions.switch();
+  kb.stroke("ctrl", "alt", "R");
+end
+
+actions.ff = function ()
+  actions.switch();
+  kb.stroke("ctrl", "alt", "F");
+end
+
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Unified-Remote-ProgDVB
-Unified Remote ProgDVB/TV
+# Custom Remote ProgDVB для Unified Remote
+
+Управление ProgDVB через Android, iPhone или Windows Phone
+
+## Функционал
+* Запуск/Выключение ProgDVB одной кнопкой
+* Управление реализовано через горячии клавиши
+* Скрипт учитывает что окно ProgDVB может быть не активым
+* Реализованы практически все возможности управления ProgDVB, в т.ч. PIP, TimeShift, запись, управление соотношением стором и размером картинки 
+
+## Установка
+
+1. Установить Unified Remote на компьютер с ProgDVB
+
+2. Содержимое папки ProgDVB положить в каталог:
+C:\ProgramData\Unified Remote\Remotes\Bundled\ProgDVB
+
+3. Предполагается, что ProgDVB установлен в стандарный каталог:
+C:\Program Files\ProgDVB
+Если папка установки другая, то необходимо подправить файл remote.lua
+
+4. Для управления ProgDVB использую стандартные хоткеи но некоторые нужно добавить вручную:
+Меню Settings/Options/Controls/
+Нажать кнопку Bind Keys и назначить такие хоткеи:
+Play	- Atl Ctrl P
+Pause	- Atl Ctrl S
+Stop	- Atl Ctrl B
+Previous	- Atl Ctrl R
+Next	- Atl Ctrl F
+
+5. Установить Unified Remote Full на мобильный телефон/планшет и добавить из раздела Media, пульт ProgDVB
